a68e0b503e429bad8b0d1f90fe5659aeebb4a24e
onadegibert <onadegibert@gmail.com>
2022-05-28 22:24:50 +0200

    added levenstein distance and api

--- src/back/Search.py
+++ src/back/Search.py
@@ -3,8 +3,9 @@ import pyproj
 import numpy as np
 from geopy.distance import geodesic
 from ast import literal_eval
+import Levenshtein
 
-PLACES= pd.read_csv('src/data/places.tsv', delimiter='\t')
+PLACES= pd.read_csv('../data/places.tsv', delimiter='\t')
 
 def get_distance_matches(coordinates1,places_coordinates, max_dist):
   #inspiration from https://gis.stackexchange.com/questions/260992/how-do-i-convert-wgs84-lat-long-points-from-degrees-to-meters-in-python
@@ -15,29 +16,53 @@ def get_distance_matches(coordinates1,places_coordinates, max_dist):
     if distance < max_dist:
       distance_matches.append(place_id)
   return (distance_matches)
+
+def get_category_matches(categories, distance_matches):
+  category_matches = []
+  for category in categories:
+    for index in distance_matches:
+      if PLACES.loc[index].Categories[category][0] != 0:
+        category_matches.append(PLACES.loc[index].to_dict())
+  #category_matches = [PLACES.loc[index].to_dict() for index in distance_matches if literal_eval(PLACES.loc[index].Categories_Id) & categories]
+  #Returns a list of dictionaries
+  return(category_matches)
   
 def return_match_spots(lat, long, categories, max_dist):
   places_coordinates = list(zip(PLACES["Id"], PLACES["Latitude"], PLACES["Longitude"]))
   distance_matches = get_distance_matches((lat, long), places_coordinates, max_dist)
-  category_matches = [index for index in distance_matches if literal_eval(PLACES.loc[index].Categories_Id) & set(categories)]
-  print(category_matches)
+  category_matches = get_category_matches(categories, distance_matches)
+  return(str(category_matches))
   
 def query(query_term, categories, max_dist=500):
   if isinstance(query_term, str): #We assume the query term is a name
-    # TODO: Include levenstein distance
-    lat = PLACES.set_index("Name").loc[query_term].Latitude
-    long = PLACES.set_index("Name").loc[query_term].Longitude
-    return_match_spots(lat, long, categories, max_dist)
+    query_term = query_term.replace("_"," ")    
+    if query_term in PLACES["Name"].values:
+      lat = PLACES.set_index("Name").loc[query_term].Latitude
+      long = PLACES.set_index("Name").loc[query_term].Longitude
+      category_matches = return_match_spots(lat, long, categories, max_dist)
+    else:
+      category_matches = "This query didn't return any result!"
+      for place in PLACES["Name"].values:
+        med = Levenshtein.distance(query_term, place)
+        if med < 6:
+          lat = PLACES.set_index("Name").loc[place].Latitude
+          long = PLACES.set_index("Name").loc[place].Longitude
+          category_matches = return_match_spots(lat, long, categories, max_dist)
   elif isinstance(query_term, tuple): #We assume those are the latitude and longitude
     lat, long = query_term
-    return_match_spots(lat, long, categories, max_dist)
+    category_matches = return_match_spots(lat, long, categories, max_dist)
   else:
-    print("This query didn't return any result!")
+    category_matches = "Please, entry a valid query!"
+  return(category_matches)
 
 def main():
   query((41.43050169245379, 2.18336528460343), {0}, 500) #return 0,2
   query((41.43050169245379, 2.18336528460343), {1}, 500) # return 1
   query((41.43050169245379, 2.18336528460343), {2}, 500) # return 0
   query("Jardí de la Julieta",{1}, 200)
-
+  query("Jardi de la Julieta",{2}, 500)
+  query("caca",{0}, 500)
+  
 main()
+
+#http://127.0.0.1:5000/search?lat=41.43050169245379&long=2.18336528460343&dist=500&categories=0
--- src/back/api.py
+++ src/back/api.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, request
+from ast import literal_eval
+from Search import *
 
 app = Flask(__name__)
 
-@app.route("/search")
-def search_spaces():
-    return "<p>Hello, World!</p>"
+@app.route("/search", methods=['GET'])
+def search():
+    args = request.args
+    values = args.to_dict()
+    return query(literal_eval(values["query"]),literal_eval(values['categories']),int(values['dist']))
