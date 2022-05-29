6048265ba227fb2ac3da1ad6dd4e7c5caed8ccf8
MayuRuru <aruizrull@protonmail.com>
2022-05-29 13:08:22 +0200

    correct map

--- src/frontmapa/src/components/Map.js
+++ src/frontmapa/src/components/Map.js
@@ -2,26 +2,42 @@ import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import styles from "./Map.module.css";
 import Checkbox from "./Checkbox";
-
-/* import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import api from "../globalapi"; */
 
 export const Map = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [places, setPlaces] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(false);
+    axios.get("http://localhost:5000/places").then((res) => {
+      setPlaces(res.data);
+    });
+  }, [setIsLoading]);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
   return (
     <div className={styles.mapbox}>
       <MapContainer
-        center={[41.42759, 2.18476]}
+        center={[41.4275103, 2.1833535]}
         zoom={25}
-        scrollWheelZoom={true}
+        scrollWheelZoom={false}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[41.42759, 2.18476]}>
+        <Marker position={[places.Latitude, places.Longitude]}>
           <Popup>
-            <Checkbox />
+            <Checkbox title={places.Name} />
           </Popup>
         </Marker>
       </MapContainer>
